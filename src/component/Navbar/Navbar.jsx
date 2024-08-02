b048b8be62ed040b294941bbb33ad8e45ba7210d
WaseemBhrath <waseembhrath514@gmail.com>
2024-08-02 12:44:55 -0700

    commit code

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '../../assets/Portfolio logo.png'
+
+export default function Navbar() {
+  return (
+    <nav>
+          <img src={require('../assets/logo.png')} alt="Logo" />
+    </nav>
+  )
+}
