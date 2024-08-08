1ed1a6a362766d6ae9ad7a97fbb7be47fc8599d5
WaseemBhrath <waseembhrath514@gmail.com>
2024-08-08 12:35:06 -0700

    commit

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
-import Logo from '../../assets/Portfolio logo.png'
+import Logo from '../../assets/Waseem Bhrath.png'
 
 export default function Navbar() {
+  const NavMainu=[
+    {
+      id: 1,
+      title:"Services",
+      link:"#",
+    },
+    {
+      id: 2,
+      title:"Works",
+      link:"#",
+    },
+    {
+      id: 3,
+      title:"Blog",
+      link:"#",
+    }
+  ]
   return (
-    <nav>
-          <img src={require('../assets/logo.png')} alt="Logo" />
-    </nav>
+    <div className="container">
+      <div className=''>
+        <img className='h-[150px]' src={Logo} alt="" />
+      </div>
+      <div>
+       <ul>
+          {NavMainu.map((item)=>{
+            return(
+              <li key = {item.id} > <a href={item.link}>{item.title}</a></li>
+            )
+
+          })}
+        </ul>
+      </div>
+    </div>
   )
 }
